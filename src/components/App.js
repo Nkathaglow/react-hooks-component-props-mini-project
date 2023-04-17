f28ad02dc0ba473c5f833a203925f78402d3d989
Nkatha_Glory <kinyua6945@gmail.com>
2023-04-18 00:25:47 +0300

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,41 @@
+
 import React from "react";
 import blogData from "../data/blog";
+import Header from './Header';
+import About from './About';
+import ArticleList from './Articlelist';
 
 console.log(blogData);
 
 function App() {
+  const blogName = 'Underreacted';
+  const aboutText = 'Welcome to my blog!';
+
+  const posts = [
+    {
+      id: 1,
+      title: 'The WET Codebase',
+      date: 'April 1, 2023',
+      preview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+    {
+      id: 2,
+      title: 'Goodbye, Clean Code',
+      preview: 'Praesent faucibus metus sed nibh auctor, sit amet maximus purus feugiat.'
+    },
+    {
+      id: 3,
+      title: 'My Decade in Review',
+      date: 'March 1, 2023',
+      preview: 'Sed dignissim ipsum ac sapien laoreet, at malesuada sapien dignissim.'
+    }
+  ];
+
   return (
-    <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+    <div>
+      <Header blogName={blogName} />
+      <About aboutText={aboutText} />
+      <ArticleList posts={posts} />
     </div>
   );
 }
